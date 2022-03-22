069f35208ad522e9482ebd6b7937c78a9d8ed919
gm <gm>
2022-03-22 09:06:38 +0100

    comment for offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1449,8 +1449,8 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -2
-#define Z_MIN_POS 0
+#define Y_MIN_POS -2 //TODO MARIO Values anpassen.
+#define Z_MIN_POS 0 
 #define X_MAX_POS X_BED_SIZE + 20
 #define Y_MAX_POS Y_BED_SIZE +3
 //#define Z_MAX_POS 385
