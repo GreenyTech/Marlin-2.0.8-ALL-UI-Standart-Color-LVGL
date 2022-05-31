a11c26cde3e8516f68c51e255efb6c33e9df01f5
gm <gm>
2022-05-31 14:41:16 +0200

    rename Temperature to temperature PID

--- Marlin/src/lcd/menu/menu_configuration.cpp
+++ Marlin/src/lcd/menu/menu_configuration.cpp
@@ -518,7 +518,7 @@ void menu_configuration() {
     #endif
 
   //SUBMENU(MSG_ADVANCED_SETTINGS, menu_advanced_settings);
-  SUBMENU(MSG_TEMPERATURE, menu_advanced_temperature);//added from the menu advanced settings
+  SUBMENU(MSG_TEMPERATURE_PID, menu_advanced_temperature);//added from the menu advanced settings
   
   //menu_advanced_temperature();
 
